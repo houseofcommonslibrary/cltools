12f8ade06cd025e6f218effefc5ea6c04e64f526
Oliver Hawkins <oli@olihawkins.com>
2019-05-29 21:43:48 +0100

    0.9.1 - Added na.rm option to value_counts

--- R/counts.R
+++ R/counts.R
@@ -1,17 +1,19 @@
 #' Count the frequency of discrete values in a vector
 #'
 #' Calculates the frequency of discrete values in a vector and returns the
-#' figures as a tibble. The results can be sorted by value or by count.
+#' counts as a tibble. The results can be sorted by value or by count.
 #'
 #' @param values A vector of values from which frequencies will be calculated.
-#' @param by The column by which to sort the results. Must be one of either
-#'   "count" or "value". The default is "count".
-#' @param order The order in which to sort the results. Must be one of either
-#'   "asc" or "desc". The default depends on the value of the \code{by}
-#'   argument. If \code{by} is "count" the default \code{order} is "desc". If
-#'   \code{by} is "value" the default \code{order} is "asc".
-#' @param name The column name to use for the values column in the results. By
-#'   default, the variable name of the input vector is used.
+#' @param by The column name by which to sort the results as a string. Must be
+#'   one of either "count" or "value". The default is "count".
+#' @param order The order in which to sort the results as a string. Must be
+#'   one of either "asc" or "desc". The default depends on the value of the
+#'   \code{by} argument. If \code{by} is "count" the default \code{order} is
+#'   "desc". If \code{by} is "value" the default \code{order} is "asc".
+#' @param name The column name to use for the values column in the results as
+#'   a string. By default, the variable name of the input vector is used.
+#' @param na.rm A boolean indicating whether to exclude NAs from the result.
+#'   The default is FALSE.
 #' @return A tibble showing the frequency of each value in the input vector.
 #' @export
 
@@ -19,7 +21,8 @@ value_counts <- function(
     values,
     by = "count",
     order = ifelse(by == "value", "asc", "desc"),
-    name = deparse(substitute(values))) {
+    name = deparse(substitute(values)),
+    na.rm = FALSE) {
 
     # Check arguments are valid
     if (! by %in% c("count", "value")) {
@@ -28,6 +31,10 @@ value_counts <- function(
 
     if (! order %in% c("asc", "desc")) {
         stop("Invalid \"by\" argument. Must be either \"asc\" or \"desc\".")
+    }
+
+    if (! is.logical(na.rm)) {
+        stop("Invalid \"na.rm\" argument. Must be either TRUE or FALSE.")
     }
 
     # If the name is a dataframe column name, extract the column name
@@ -39,8 +46,13 @@ value_counts <- function(
     by_col <- ifelse(by == "count", "count", name)
     order_func <- ifelse(order == "desc", dplyr::desc, function(d){d})
 
+    # Set option for handling NAs
+    use_na <- ifelse(na.rm, "no", "ifany")
+
     # Create the results dataframe and return
-    tibble::as_tibble(table(values), .name_repair = ~ c(name, "count")) %>%
+    tibble::as_tibble(
+            table(values, useNA = use_na),
+            .name_repair = ~ c(name, "count")) %>%
         dplyr::arrange(order_func(.data[[by_col]])) %>%
         dplyr::mutate(percent = .data$count / sum(.data$count))
 }
